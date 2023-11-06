a49bf95d2d312744700188598f48f5d5948daccb
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-06 14:34:41 +0300

    component: create new task form

---
+++ src/components/CreateTaskForm/CreateTaskForm.tsx
@@ -0,0 +1,136 @@
+'use client'
+import React, { useState } from 'react'
+import toast from 'react-hot-toast'
+import { Button } from '../Button/Button'
+import { StyledCreateTaskForm } from './styles'
+import { useAppContext } from '@/context'
+import { add } from '@/helpers/icons'
+import axios from 'axios'
+
+const CreateTaskForm = () => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const [date, setDate] = useState('')
+  const [completed, setCompleted] = useState(false)
+  const [important, setImportant] = useState(false)
+
+  const { theme, getTasks, closeModal } = useAppContext()
+
+  const handleChange = (name: string) => (e: any) => {
+    switch (name) {
+      case 'title':
+        setTitle(e.target.value)
+        break
+      case 'description':
+        setDescription(e.target.value)
+        break
+      case 'date':
+        setDate(e.target.value)
+        break
+      case 'completed':
+        setCompleted(e.target.checked)
+        break
+      case 'important':
+        setImportant(e.target.checked)
+        break
+      default:
+        break
+    }
+  }
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+
+    try {
+      const call = await fetch('/api', {
+        method: 'POST',
+        body: JSON.stringify({
+          title,
+          description,
+          date,
+          completed,
+          important,
+        }),
+      })
+      console.log('call:', call)
+
+      if (!call.ok) {
+        toast.error('Failed to add new task')
+      }
+
+      toast.success('Task created successfully.')
+      getTasks()
+      closeModal()
+    } catch (error) {
+      toast.error('Something went wrong.')
+      console.log(error)
+    }
+  }
+
+  return (
+    <StyledCreateTaskForm onSubmit={handleSubmit} theme={theme}>
+      <h1>Create a Task</h1>
+      <div className='input-control'>
+        <label htmlFor='title'>Title</label>
+        <input
+          type='text'
+          id='title'
+          value={title}
+          name='title'
+          onChange={handleChange('title')}
+          placeholder='e.g, Watch a video from Fireship.'
+        />
+      </div>
+      <div className='input-control'>
+        <label htmlFor='description'>Description</label>
+        <textarea
+          value={description}
+          onChange={handleChange('description')}
+          name='description'
+          id='description'
+          rows={4}
+          placeholder='e.g, Watch a video about Next.js Auth'
+        ></textarea>
+      </div>
+      <div className='input-control'>
+        <label htmlFor='date'>Date</label>
+        <input value={date} onChange={handleChange('date')} type='date' name='date' id='date' />
+      </div>
+      <div className='input-control toggler'>
+        <label htmlFor='completed'>Toggle Completed</label>
+        <input
+          value={completed.toString()}
+          onChange={handleChange('completed')}
+          type='checkbox'
+          name='completed'
+          id='completed'
+        />
+      </div>
+      <div className='input-control toggler'>
+        <label htmlFor='important'>Toggle Important</label>
+        <input
+          value={important.toString()}
+          onChange={handleChange('important')}
+          type='checkbox'
+          name='important'
+          id='important'
+        />
+      </div>
+
+      <div className='submit-btn flex justify-end'>
+        <Button
+          type='submit'
+          name='Create Task'
+          icon={add}
+          padding={'0.8rem 2rem'}
+          borderRad={'0.8rem'}
+          fw={'500'}
+          fs={'1.2rem'}
+          background={'rgb(0, 163, 255)'}
+        />
+      </div>
+    </StyledCreateTaskForm>
+  )
+}
+
+export { CreateTaskForm }
