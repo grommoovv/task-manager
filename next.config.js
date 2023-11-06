8ae42aba6e0fd5d419cd7b948c966bbd0c16926f
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-06 14:32:42 +0300

    next cfg

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  images: {
+    domains: ['localhost', 'https://img.clerk.com', 'img.clerk.com'],
+  },
+}
 
 module.exports = nextConfig
